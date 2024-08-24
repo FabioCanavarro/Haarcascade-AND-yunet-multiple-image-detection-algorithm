387dbf2443ffaab87f8df02580288af2c4edea51
Fabio Canavarro <fabiocanavarrotoh@gmail.com>
2024-08-24 21:48:54 +0700

    Yunet
    whoops forget to commit

---
+++ main/testyunet.py
@@ -0,0 +1,85 @@
+import cv2 as cv
+import numpy as np
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from deepface import DeepFace as df
+import multiprocessing as mp
+class YuNet:
+    def __init__(self, modelPath, inputSize=[320, 320], confThreshold=0.6, nmsThreshold=0.3, topK=5000, backendId=0, targetId=0):
+        self._modelPath = modelPath
+        self._inputSize = tuple(inputSize) # [w, h]
+        self._confThreshold = confThreshold
+        self._nmsThreshold = nmsThreshold
+        self._topK = topK
+        self._backendId = backendId
+        self._targetId = targetId
+
+        self._model = cv.FaceDetectorYN.create(
+            model=self._modelPath,
+            config="",
+            input_size=self._inputSize,
+            score_threshold=self._confThreshold,
+            nms_threshold=self._nmsThreshold,
+            top_k=self._topK,
+            backend_id=self._backendId,
+            target_id=self._targetId)
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def setBackendAndTarget(self, backendId, targetId):
+        self._backendId = backendId
+        self._targetId = targetId
+        self._model = cv.FaceDetectorYN.create(
+            model=self._modelPath,
+            config="",
+            input_size=self._inputSize,
+            score_threshold=self._confThreshold,
+            nms_threshold=self._nmsThreshold,
+            top_k=self._topK,
+            backend_id=self._backendId,
+            target_id=self._targetId)
+
+    def setInputSize(self, input_size):
+        self._model.setInputSize(tuple(input_size))
+
+    def infer(self, image):
+        # Forward
+        faces = self._model.detect(image)
+        return np.array([]) if faces[1] is None else faces[1]
+    
+def visualize(image, results, box_color=(0, 255, 0), text_color=(0, 0, 255)):
+    output = image.copy()
+    landmark_color = [
+        (255,   0,   0), # right eye
+        (  0,   0, 255), # left eye
+        (  0, 255,   0), # nose tip
+        (255,   0, 255), # right mouth corner
+        (  0, 255, 255)  # left mouth corner
+    ]
+    for det in results:
+        bbox = det[0:4].astype(np.int32)
+        cv.rectangle(output, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), box_color, 2)
+
+        conf = det[-1]
+        cv.putText(output, '{:.4f}'.format(conf), (bbox[0], bbox[1]+12), cv.FONT_HERSHEY_DUPLEX, 0.5, text_color)
+
+        landmarks = det[4:14].astype(np.int32).reshape((5,2))
+        for idx, landmark in enumerate(landmarks):
+            cv.circle(output, landmark, 2, landmark_color[idx], 2)
+    return output
+
+model = YuNet(modelPath=r"models\face_detection_yunet_2023mar.onnx",inputSize=[640, 480])
+frame = cv2.imread(r"C:\Users\ASUS\Downloads\imgsss\wp11500678-solvay-conferences-wallpapers.jpg")
+h, w, _ = frame.shape
+model.setInputSize([w, h])
+detected = visualize(frame,model.infer(frame))
+cv2.imshow(f"frame",detected)
+while True:
+    a = cv2.waitKey(1) & 0XFF
+    if a == 27:
+        cv2.destroyAllWindows()
+        break
